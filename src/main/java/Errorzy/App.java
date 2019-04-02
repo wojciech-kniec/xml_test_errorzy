748edcdf44440b9b6df3a4c318e65aa82a99f571
wojciech-kniec <kniecwojciech@gmail.com>
2019-04-02 17:03:07 +0200

    city lista dodana linked list

--- src/main/java/Errorzy/App.java
+++ src/main/java/Errorzy/App.java
@@ -7,7 +7,9 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * temp main
@@ -35,20 +37,34 @@ public class App {
                 "       \"*==*\"\"                             ^\"*==*\"\"   ");
         System.out.println("");
 
-        File file = new File("src/nextbike-live_polska.xml");
+        File file = new File("nextbike-official.xml");
         JAXBContext jaxbContext = JAXBContext.newInstance(Markers.class);
         Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
         Markers markerslist = (Markers) jaxbUnmarshaller.unmarshal(file);
-        System.out.println(markerslist.getCountryList().get(0).getCityList().get(0).getPlaceList().get(0).getBikeList().get(0));
+        System.out.println(markerslist.getCountryList());
+
 
         FindCountry findCountry = new FindCountry();
-        findCountry.returnCountryList();
+//        findCountry.returnCountryList();
         List<City> cities = new ArrayList<>();
-        cities = markerslist.getCountryList().get(0).getCityList();
+
+
+        cities = markerslist.getCountryList().getClass();
+        for (City city : cities) {
+            System.out.println(city.getName());
+        }
         List<Place> places = new ArrayList<>();
         places = cities.get(0).getPlaceList();
         List<Bike> bikes = new ArrayList<>();
         bikes = places.get(0).getBikeList();
 
+    }
+
+    private List<City> getCities(List<Country> countiesData) {
+        List<City> allCities = new LinkedList<>();
+        for (Country c : countiesData) {
+            allCities.addAll(c.getCityList());
+        }
+        return allCities;
     }
 }
